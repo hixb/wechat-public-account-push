cffdc9b8c863da27e1fa1af2c11ee6833ad1b9d1
朱晓冰 <zhuxb@tuju.cn>
2022-10-26 16:46:52 +0800

    perf: code style

--- src/sendMsg.ts
+++ src/sendMsg.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 export const sendMsg = (params: any) => {
-  return axios.post(
-    `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${params["access_token"]}`,
-    {
+  return axios({
+    method: "post",
+    url: `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${params["access_token"]}`,
+    data: {
       ...params,
       data: params["data"] || {}
     }
-  )
+  });
 }
