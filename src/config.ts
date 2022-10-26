dde3f7ee2f428f92c29d074cc72c4e5634e24a59
hixb <Hello16369@163.com>
2022-10-26 20:35:07 +0800

    docs: Change parameters

--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,7 @@ import {
 const weChatParams: IWeChatParams = {
   appid: "wx475f1615da469ac3",
   secret: "dee6fcb93ff3b2bba802753bb2a9840a",
-  touser: "oAXSp6GvKRcy1UaPKaUDPLcV3ByI",
+  touser: "oAXSp6Dafux_aZQtgQ_J-wQqodYM",
   template_id: "ihL9WKJ8ZPprPnWNLLVL_9lhErusymZdI382YZKoLpA"
 }
 
@@ -37,7 +37,7 @@ const weatherParams: IWeatherParams = {
 const executionTimeParams: IImplementParams = {
   interval: 1, // 间隔天数, 间隔为整数
   runNow: false, // 是否立即运行
-  time: "09:39:00" // 执行的时间点 时在0~23之间
+  time: "09:00:00" // 执行的时间点 时在0~23之间
 }
 
 // 天行数据api => https://www.tianapi.com/console/
